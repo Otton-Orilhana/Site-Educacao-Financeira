fb0c31b30ffa13e3037eaff98add348cd52ca941
Otton Orilhana <ottonhyorilhana@gmail.com>
2026-02-28 09:38:52 -0300

    Limpeza: remove caches e DB do controle; atualiza .gitignore

--- Backend/auth.py
+++ Backend/auth.py
@@ -1,14 +1,14 @@
-# auth.py
+﻿# auth.py
 import os
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
-from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from database import get_db
 import models
 
+from passlib.hash import pbkdf2_sha256
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -16,14 +16,13 @@ SECRET_KEY = os.getenv("JWT_SECRET", "troque_este_seguro")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "120"))
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
-def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pbkdf2_sha256.verify(plain_password, hashed_password)
 
-def get_password_hash(password):
-    return pwd_context.hash(password)
+def get_password_hash(password: str) -> str:
+    return pbkdf2_sha256.hash(password)
 
 def create_access_token(subject: str | int, expires_delta: timedelta | None = None):
     to_encode = {"sub": str(subject)}
